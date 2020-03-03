17bbb4bdd7f72fa50337110627e64c5c4941ac0a
Jason Lowe <jlowe000@gmail.com>
2020-03-03 12:34:36 +1000

    added TANDM to the spark data load and tested augmented the task as primary and secondary task

--- spark/src/main/java/viz/load/SparkLoad.java
+++ spark/src/main/java/viz/load/SparkLoad.java
@@ -22,6 +22,9 @@ import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.api.java.UDF1;
 import org.apache.spark.sql.functions;
 import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.explode;
+import static org.apache.spark.sql.functions.posexplode;
+import static org.apache.spark.sql.functions.split;
 import org.apache.spark.sql.types.StructType;
 import org.apache.spark.sql.types.DataType;
 import org.apache.spark.sql.types.DataTypes;
@@ -39,6 +42,7 @@ import org.apache.spark.ml.feature.FeatureHasher;
 import org.apache.spark.ml.classification.LogisticRegression;
 import org.apache.spark.ml.classification.LogisticRegressionModel;
 import org.apache.spark.ml.classification.BinaryLogisticRegressionTrainingSummary;
+import org.apache.spark.sql.catalyst.encoders.RowEncoder;
 
 /**
  *
@@ -61,6 +65,52 @@ public class SparkLoad {
       .save();
   }
 
+  private static void importTANDMToDB(SparkSession spark, String file) {
+    List<org.apache.spark.sql.types.StructField> listOfStructField = new ArrayList<org.apache.spark.sql.types.StructField>();
+    listOfStructField.add(DataTypes.createStructField("primary", DataTypes.StringType, true));
+    listOfStructField.add(DataTypes.createStructField("second", DataTypes.StringType, true));
+    StructType structType = DataTypes.createStructType(listOfStructField);
+
+    Dataset<Row> csvData = spark.read()
+	                        .option("header","true")
+	                        .option("inferSchema","true")
+				.csv(file).cache();
+    for (String ofn : csvData.schema().fieldNames()) {
+      String nfn = ofn.toLowerCase().replaceAll("[^a-zA-Z0-9]","_");
+      csvData = csvData.withColumnRenamed(ofn,nfn);
+    }
+
+    Dataset<Row> masterData = csvData.select("action","id","start_time","end_time","hour_of_day","duration_sec","duration_hhmmss","observer","observed","location");
+    masterData.schema().printTreeString();
+    masterData.write()
+      .format("jdbc")
+      .option("url","jdbc:postgresql:viz4socialgood")
+      .option("user", "viz4socialgood")
+      .option("password", "viz4socialgood")
+      .option("dbtable", "TANDM_MASTER")
+      .mode(SaveMode.Overwrite)
+      .save();
+
+    Dataset<Row> taskData = csvData.select(col("action"),col("id"),col("observer"),col("task"))
+	                           .withColumn("primary_task", split(col("task"),"\\|").getItem(0))
+                                   .withColumn("secondary_task", split(col("task"),"\\|").getItem(1))
+				   .drop(col("task"));
+    for (String ofn : taskData.schema().fieldNames()) {
+      String nfn = ofn.toLowerCase().replaceAll("[^a-zA-Z0-9]","_");
+      taskData = taskData.withColumnRenamed(ofn,nfn);
+    }
+
+    taskData.schema().printTreeString();
+    taskData.write()
+      .format("jdbc")
+      .option("url","jdbc:postgresql:viz4socialgood")
+      .option("user", "viz4socialgood")
+      .option("password", "viz4socialgood")
+      .option("dbtable", "TANDM_TASK")
+      .mode(SaveMode.Overwrite)
+      .save();
+  }
+
   private static void importFileToDB(SparkSession spark, String file, String name, String col, String... cols) {
     Dataset<Row> csvData = spark.read()
 	                        .option("header","true")
@@ -76,7 +126,6 @@ public class SparkLoad {
     } else {
       outputData = csvData;
     }
-    // outputData = outputData.select(Arrays.asList(outputData.schema().fieldNames()).stream().map(x -> x.toLowerCase().replaceAll("[^a-zA-Z0-9]","_")).toArray(size -> new String[size]));
     for (String ofn : outputData.schema().fieldNames()) {
       String nfn = ofn.toLowerCase().replaceAll("[^a-zA-Z0-9]","_");
       outputData = outputData.withColumnRenamed(ofn,nfn);
@@ -91,19 +140,22 @@ public class SparkLoad {
       .mode(SaveMode.Overwrite)
       .save();
   }
+
   public static void testFile() throws Exception {
-    String file1 = "/home/jlowe000/repos/sunnystreet/data/sunny-street-patients.csv"; // Should be some file on your system
-    String file2 = "/home/jlowe000/repos/sunnystreet/data/sunny-street-patient-diagnosis.csv"; // Should be some file on your system
-    String file3 = "/home/jlowe000/repos/sunnystreet/data/sunny-street-patient-medicine.csv"; // Should be some file on your system
-    String file4 = "/home/jlowe000/repos/sunnystreet/data/sunny-street-patient-immunisation.csv"; // Should be some file on your system
-    String file5 = "/home/jlowe000/repos/sunnystreet/data/campfire-shift-measures.csv"; // Should be some file on your system
+    // String file1 = "/home/jlowe000/repos/sunnystreet/data/sunny-street-patients.csv";
+    // String file2 = "/home/jlowe000/repos/sunnystreet/data/sunny-street-patient-diagnosis.csv";
+    // String file3 = "/home/jlowe000/repos/sunnystreet/data/sunny-street-patient-medicine.csv";
+    // String file4 = "/home/jlowe000/repos/sunnystreet/data/sunny-street-patient-immunisation.csv";
+    // String file5 = "/home/jlowe000/repos/sunnystreet/data/campfire-shift-measures.csv";
+    String file6 = "/home/jlowe000/repos/sunnystreet/data/sunny-street-tandm.csv";
     SparkSession spark = SparkSession.builder().appName("Simple Application").config("spark.master","local").getOrCreate();
 
-    importFileToDB(spark,file1,"PATIENTS","");
-    importFileToDB(spark,file2,"PATIENT_DIAGNOSIS","");
-    importFileToDB(spark,file3,"PATIENT_MEDICINE","");
-    importFileToDB(spark,file4,"PATIENT_IMMUNISATION","");
-    importFileToDB(spark,file5,"SHIFT_MEASURES","");
+    // importFileToDB(spark,file1,"PATIENTS","");
+    // importFileToDB(spark,file2,"PATIENT_DIAGNOSIS","");
+    // importFileToDB(spark,file3,"PATIENT_MEDICINE","");
+    // importFileToDB(spark,file4,"PATIENT_IMMUNISATION","");
+    // importFileToDB(spark,file5,"SHIFT_MEASURES","");
+    importTANDMToDB(spark,file6);
 
     spark.stop();
   }
